c8f3707e41183bd5ca72c91860a9cffb267b5c7b
Javier Cunat <22517@columbushs.com>
2019-11-15 08:45:53 -0500

    Mars Rover Started

--- mars-rover-v2/javascripts/app.js
+++ mars-rover-v2/javascripts/app.js
@@ -1,15 +1,69 @@
 // Rover Object Goes Here
 // ======================
+let rover ={
+  direction = "N",
+  x:0,
+  y:0,
+};
 
 // ======================
 function turnLeft(rover){
   console.log("turnLeft was called!");
 }
+if(rover.direction == "N"){
+  roverDirection = "W"
+}
+else if(rover.direction == "W"){
+  roverDirection ="S"
+}
+else if(rover.direction == "S"){
+  roverDirection = "E"
+}
+else if(rover.direction == "E"){
+  roverDirection = "N"
+};
+
+
 
 function turnRight(rover){
   console.log("turnRight was called!");
 }
+if(rover.direction =="N"){
+  roverDirection ="E"
+}
+else if(rover.direction =="E"){
+  roverDirection =="S"
+}
+else if(rover.direction =="S"){
+  roverDirection =="W"
+}
+else if(rover.direction =="W"){
+  roverDirection ="N"
+};
+
 
 function moveForward(rover){
   console.log("moveForward was called")
 }
+if(rover.direction =="N"){
+  rover.y =
+  rover.x =
+}
+
+if else(rover.direction =="N"){
+  rover.y =
+  rover.x =
+}
+
+if else (rover.direction =="N"){
+  rover.y =
+  rover.x =
+}
+
+if else(rover.direction =="N"){
+  rover.y =
+  rover.x =
+}
+
+
+
